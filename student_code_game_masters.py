c132c6c90fdf33b1e36df73e5d47921c37939d98
Ava Robinson <avamrobinson@gmail.com>
2019-02-24 03:53:17 -0600

    first commit to git #commitmentissues

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -33,8 +33,39 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### student code goes here
-        pass
+        peg1=[]
+        peg2=[]
+        peg3=[]
+
+        #finding what is on peg1
+        listOfBindings=self.kb.kb_ask(parse_input('fact: (on ?disk peg1)')) #returns a list of bindings
+        #for each element in the list of bindigns we want to get the constant
+        if listOfBindings:
+            for bindings in listOfBindings:
+                binding = bindings.bindings_dict['?disk']  # return a binding object
+                diskNumber = int(binding[-1])
+                peg1.append(diskNumber)
+
+        #finding what is on peg2
+        listOfBindings = self.kb.kb_ask(parse_input('fact: (on ?disk peg2)'))  # returns a list of bindings
+        # for each element in the list of bindigns we want to get the constant
+        if listOfBindings:
+            for bindings in listOfBindings:
+                binding=bindings.bindings_dict['?disk'] #return a binding object
+                diskNumber = int(binding[-1])
+                peg2.append(diskNumber)
+
+        #finding whats on peg3
+        listOfBindings = self.kb.kb_ask(parse_input('fact: (on ?disk peg3)'))  # returns a list of bindings
+        # for each element in the list of bindigns we want to get the constant
+        if listOfBindings:
+            for bindings in listOfBindings:
+                binding = bindings.bindings_dict['?disk']  # return a binding object
+                diskNumber = int(binding[-1])
+                peg3.append(diskNumber)
+
+        game_state=(tuple(sorted(peg1)), tuple(sorted(peg2)), tuple(sorted(peg3)))
+        return game_state
 
     def makeMove(self, movable_statement):
         """
@@ -52,8 +83,48 @@ class TowerOfHanoiGame(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
-        pass
+        # Student code goes here
+        matches = match(movable_statement, (self.produceMovableQuery()).statement)
+        if matches:
+
+            disk = matches.bindings[0].constant.element # disk that we are trying to move
+            start = matches.bindings[1].constant.element # starting peg
+            end = matches.bindings[2].constant.element # end peg
+
+            # retract on statement
+            self.kb.kb_retract(parse_input('fact: (on ' + disk + ' ' + start + ')'))
+            # retract the top statement
+            self.kb.kb_retract(parse_input('fact: (top ' + disk + ' ' + start + ')'))
+
+            if not self.kb.kb_ask(parse_input('fact: (above ' + disk + ' ?diskb)')):  # if start will b empty
+                self.kb.kb_assert((parse_input('fact: (empty ' + start + ')')))  # make the start peg empty
+            else: # if the start is not empty then we have to assert a new top on the start peg
+                listOfBindings = self.kb.kb_ask(parse_input('fact: (above ' + disk + ' ?diskb)'))
+                new_top = listOfBindings[0].bindings_dict['?diskb']  # return a disk object
+                self.kb.kb_assert(parse_input('fact: (top ' + new_top + ' ' + start + ')'))  # assert new top for start peg
+                self.kb.kb_retract(parse_input('fact: (above ' + disk + ' ' + new_top + ')'))
+
+            # if the peg we are moving to is empty then we have to retract the empty fact
+            if self.kb.kb_ask(parse_input('fact: (empty ' + end + ')')):
+                self.kb.kb_retract(parse_input('fact: (empty ' + end + ')'))
+                #assert new on statement
+                self.kb.kb_assert(parse_input('fact: (on ' + disk + ' ' + end + ')'))
+                #assert new top statement for end peg
+                self.kb.kb_assert(parse_input('fact: (top ' + disk + ' ' + end + ')'))
+
+            #if the peg we are moving to is not empty then we must retract the old top and assert a new one
+            else:
+                #retract the old top on the end peg
+                listOfBindings = self.kb.kb_ask(parse_input('fact: (top ?disk ' + end + ')'))
+                old_top = listOfBindings[0].bindings_dict['?disk']
+                self.kb.kb_retract(parse_input('fact: (top ' + old_top + ' ' + end + ')'))
+
+                #make new top on the target peg
+                # assert new on statement
+                self.kb.kb_assert(parse_input('fact: (on ' + disk + ' ' + end + ')'))
+                # assert new top statement for end peg
+                self.kb.kb_assert(parse_input('fact: (top ' + disk + ' ' + end + ')'))
+                self.kb.kb_assert(parse_input('fact: (above ' + disk + ' ' + old_top))
 
     def reverseMove(self, movable_statement):
         """
@@ -99,8 +170,38 @@ class Puzzle8Game(GameMaster):
         Returns:
             A Tuple of Tuples that represent the game state
         """
-        ### Student code goes here
-        pass
+        row1 = [-1, -1, -1]
+        row2 = [-1, -1, -1]
+        row3 = [-1, -1, -1]
+
+        # row1
+        listOfBindings = self.kb.kb_ask(parse_input('fact: (position ?tile ?x pos1'))
+        for binding in listOfBindings:
+            tileNumber = int((binding.bindings_dict['?tile'])[-1])
+            positionBinding = self.kb.kb_ask(parse_input('fact: (position tile' + str(tileNumber) + ' ?pos pos1'))
+            tile_x_pos = int((positionBinding[0].bindings_dict['?pos'])[-1])
+            if not tileNumber == 0:
+                row1[tile_x_pos - 1] = tileNumber
+        # row2
+        listOfBindings = self.kb.kb_ask(parse_input('fact: (position ?tile ?x pos2'))
+        for binding in listOfBindings:
+            tileNumber = int((binding.bindings_dict['?tile'])[-1])
+            positionBinding = self.kb.kb_ask(parse_input('fact: (position tile' + str(tileNumber) + ' ?pos pos2'))
+            tile_x_pos = int((positionBinding[0].bindings_dict['?pos'])[-1])
+            if not tileNumber == 0:
+                row2[tile_x_pos - 1] = tileNumber
+
+        # row3
+        listOfBindings = self.kb.kb_ask(parse_input('fact: (position ?tile ?x pos3'))
+        for binding in listOfBindings:
+            tileNumber = int((binding.bindings_dict['?tile'])[-1])
+            positionBinding = self.kb.kb_ask(parse_input('fact: (position tile' + str(tileNumber) + ' ?pos pos3'))
+            tile_x_pos = int((positionBinding[0].bindings_dict['?pos'])[-1])
+            if not tileNumber == 0:
+                row3[tile_x_pos - 1] = tileNumber
+
+        game_state = (tuple(row1), tuple(row2), tuple(row3))
+        return game_state
 
     def makeMove(self, movable_statement):
         """
@@ -118,8 +219,23 @@ class Puzzle8Game(GameMaster):
         Returns:
             None
         """
-        ### Student code goes here
-        pass
+
+        if self.isMovableLegal(movable_statement):
+            matches = match(movable_statement, (self.produceMovableQuery()).statement)
+            tile = matches.bindings[0].constant.element
+            position1 = matches.bindings[1].constant.element
+            position2 = matches.bindings[2].constant.element
+            position3 = matches.bindings[3].constant.element
+            position4 = matches.bindings[4].constant.element
+
+            # listOfBindings = self.kb.kb_ask(parse_input('fact: (position ?tile ' + position3 + ' ' + position4 + ')'))
+            # tile2 = listOfBindings[0].bindings_dict['?tile']  # returns the swapping tile
+
+            self.kb.kb_retract(parse_input('fact: (position ' + tile + ' ' + position1 + ' ' + position2 + ')'))
+            self.kb.kb_retract(parse_input('fact: (position tile0 ' + position3 + ' ' + position4 + ')'))
+
+            self.kb.kb_assert(parse_input('fact: (position ' + tile + ' ' + position3 + ' ' + position4 + ')'))
+            self.kb.kb_assert(parse_input('fact: (position tile0 ' + position1 + ' ' + position2 + ')'))
 
     def reverseMove(self, movable_statement):
         """
--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -18,13 +18,74 @@ class SolverDFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+        # Student code goes here
+        # Student code goes here
+        # DFS implemented using stack (LIFO)
+        # look at starting condition
+        # consider the movable moves from first to last
+        # for every movable look at its movables
+        # every time you move to another thing you check if it is winning condition
+
+        # fill the tree with game states
+        # look at top node
+        # get the movables, make new game state, append to children
+        # check if currentState == victory state --> return true
+        # if self.currentState.state == self.victoryCondition:
+        #    return True
+        # if not at victory condition
+        # looping through movables
+        # get movables and loop through as long as curr.children is empty and there are movables
+        # loop through movables -> make move to child --> make new gameState (depth + 1) --> set newState.parent to self.current state --> append new state to children of current
+        # reverseMove
+
+        moved = False
+        # if we have reached the victory state --> return true
+        if self.currentState.state == self.victoryCondition:
+            return True
+
+        # look at all of the possible moves and add the children (movables)
+        for move in self.gm.getMovables():
+            # for every move, make the move and get the resulting game state
+            self.gm.makeMove(move)
+            newGameState = self.gm.getGameState()
+            newChild = GameState(newGameState, self.currentState.depth + 1, move)
+            # if the move will not bring you back to the parent state --> populate this child with information
+            if not self.currentState.parent or not newGameState == self.currentState.parent.state:
+                # add child to children array
+                self.currentState.children.append(newChild)
+                # update the parent of this new child
+                newChild.parent = self.currentState
+            self.gm.reverseMove(move)
+
+        # actually make the move
+        while self.currentState.nextChildToVisit < len(self.currentState.children):
+            # if the next child to visit has not been visited yet --> visit it
+            if self.currentState.children[self.currentState.nextChildToVisit] not in self.visited:
+                # update move
+                moved = True
+                # make the move to get you to this child
+                self.gm.makeMove(self.currentState.children[self.currentState.nextChildToVisit].requiredMovable)
+                self.currentState = self.currentState.children[self.currentState.nextChildToVisit]
+                # update visited
+                self.visited[self.currentState] = True
+                if self.victoryCondition == self.currentState.state:
+                    return True
+                else:
+                    return False
+            self.currentState.nextChildToVisit = self.currentState.nextChildToVisit + 1
+
+        # if we never find a child to visit next --> go back to the parent
+        else:
+            self.currentState = self.currentState.parent
+            self.gm.reverseMove(self.currentState.requiredMovable)
 
 
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
+        self.bfsQ = deque()
+        self.bfsQ.append(self.currentState)
+        self.front = 0
 
     def solveOneStep(self):
         """
@@ -39,5 +100,4 @@ class SolverBFS(UninformedSolver):
         Returns:
             True if the desired solution state is reached, False otherwise
         """
-        ### Student code goes here
-        return True
+        # Student code goes here
