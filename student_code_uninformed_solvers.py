f59440d86ea9a6daa0dfda42baabb6cf2c9d60c6
Ava Robinson <avamrobinson@gmail.com>
2019-02-24 22:33:31 -0600

    #commitmentissues

--- student_code_uninformed_solvers.py
+++ student_code_uninformed_solvers.py
@@ -1,5 +1,5 @@
-
 from solver import *
+from collections import deque
 
 class SolverDFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
@@ -38,7 +38,7 @@ class SolverDFS(UninformedSolver):
         # loop through movables -> make move to child --> make new gameState (depth + 1) --> set newState.parent to self.current state --> append new state to children of current
         # reverseMove
 
-        moved = False
+
         # if we have reached the victory state --> return true
         if self.currentState.state == self.victoryCondition:
             return True
@@ -61,8 +61,6 @@ class SolverDFS(UninformedSolver):
         while self.currentState.nextChildToVisit < len(self.currentState.children):
             # if the next child to visit has not been visited yet --> visit it
             if self.currentState.children[self.currentState.nextChildToVisit] not in self.visited:
-                # update move
-                moved = True
                 # make the move to get you to this child
                 self.gm.makeMove(self.currentState.children[self.currentState.nextChildToVisit].requiredMovable)
                 self.currentState = self.currentState.children[self.currentState.nextChildToVisit]
@@ -83,9 +81,7 @@ class SolverDFS(UninformedSolver):
 class SolverBFS(UninformedSolver):
     def __init__(self, gameMaster, victoryCondition):
         super().__init__(gameMaster, victoryCondition)
-        self.bfsQ = deque()
-        self.bfsQ.append(self.currentState)
-        self.front = 0
+        self.q = deque()
 
     def solveOneStep(self):
         """
@@ -101,3 +97,59 @@ class SolverBFS(UninformedSolver):
             True if the desired solution state is reached, False otherwise
         """
         # Student code goes here
+        if self.currentState.state == self.victoryCondition:
+            return True
+        else:
+            self.visited[self.currentState] = True
+
+        # if we have not seen this state before --> add it to the queue
+        if self.currentState not in self.q and self.currentState:
+            self.q.append(self.currentState)
+
+        if self.gm.getMovables():
+            for move in self.gm.getMovables():
+                #make the move and make a new game state
+                self.gm.makeMove(move)
+                new_state = self.gm.getGameState()
+                new_game_state = GameState(new_state, self.currentState.depth + 1, move)
+                #update the parent
+                new_game_state.parent = self.currentState
+                #add the new state to the children
+                self.currentState.children.append(new_game_state)
+                if new_game_state not in self.visited:
+                    self.visited[new_game_state] = False
+                self.gm.reverseMove(move)
+
+            for c in self.currentState.children:
+                if c not in self.q and not self.visited[c]:
+                    self.q.append(c)
+
+        # need to remove the current node
+        self.q.popleft()
+
+        path = []
+        currentstate = self.q[0]
+        x = 0
+
+        # reversing all the way back to the top parent, changes current state
+        while self.currentState.parent:
+            self.gm.reverseMove(self.currentState.requiredMovable)
+            self.currentState = self.currentState.parent
+
+        # filling the path array with all the moves needed to get back to the parent, changes the queue state
+        while currentstate.parent:
+            path.insert(0, currentstate.requiredMovable)
+            currentstate = currentstate.parent
+
+        # make all the moves we just stored in path that are required to get down to first queue
+        while x < len(path):
+            self.gm.makeMove(path[x])
+            x += 1
+        self.currentState = self.q[0]
+
+        if self.currentState.state == self.victoryCondition:
+            return True
+        else:
+            return False
+
+
